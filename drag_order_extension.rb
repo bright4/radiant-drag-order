fe0a38deaddbb8ba692b8516963d5d82eb7ae7a8
Bright 4 <info@bright4.nl>
2009-03-17 09:52:41 +0100

    Now really compatible with Radiant > 0.6.9.

--- drag_order_extension.rb
+++ drag_order_extension.rb
@@ -2,29 +2,36 @@
 require_dependency 'application'
 
 class DragOrderExtension < Radiant::Extension
-	version "0.2"
-	description "This extension allows pages to be moved to any arbitrary (valid) place in the 
+  version "0.2.3"
+  description "This extension allows pages to be moved to any arbitrary (valid) place in the 
 document tree structure by dragging the page to its new position and dropping it there. Created by 
 Bright 4, February 2009. Inspired by and based on Sean Cribbs' Reorder extension."
-	url "http://www.bright4.nl/"
+  url "http://www.bright4.nl/"
 
-	define_routes do |map|
-		map.with_options :controller => "admin/page" do |page|
-			page.page_move_to "admin/pages/:id/move_to/:rel/:pos", :action => "move_to"
-		end
-	end
+  define_routes do |map|
+    begin
+      Admin::PageController
+      path = "admin/page"
+    rescue  
+      path = "admin/pages"
+    end
+    map.with_options :controller => path do |page|
+      page.page_move_to "admin/pages/:id/move_to/:rel/:pos", :action => "move_to"
+    end
+  end
 
-	def activate
-		admin.page.index.add :sitemap_head, "drag_order_header"
-		admin.page.index.add :node, "drag_order"
-		admin.page.index.add :top, "header"
-		Page.send :include, DragOrder::PageExtensions
-		(Admin::PageController rescue Admin::PagesController).send :helper, DragOrder::PageHelper
-		(Admin::PageController rescue Admin::PagesController).send :include, DragOrder::PageControllerExtensions
-		StandardTags.send :include, DragOrder::TagExtensions
-	end
+  def activate
+    index = (admin.page || admin.pages).index
+    index.add :sitemap_head, "admin/pages/drag_order_header"
+    index.add :node, "admin/pages/drag_order"
+    index.add :top, "admin/pages/header"
+    Page.send :include, DragOrder::PageExtensions
+    (Admin::PageController rescue Admin::PagesController).send :helper, DragOrder::PageHelper
+    (Admin::PageController rescue Admin::PagesController).send :include, DragOrder::PageControllerExtensions
+    StandardTags.send :include, DragOrder::TagExtensions
+  end
 
-	def deactivate
-	end
-	
+  def deactivate
+  end
+  
 end
